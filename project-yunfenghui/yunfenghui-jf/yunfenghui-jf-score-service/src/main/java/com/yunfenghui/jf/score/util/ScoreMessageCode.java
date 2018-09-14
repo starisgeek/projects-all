5531aa0a6d60a9ce108968ddcdc0580cbb92716b
cloud_wanghao <wanghao_oo@126.com>
2018-09-14 10:32:21 +0800

    MessageCode

---
+++ project-yunfenghui/yunfenghui-jf/yunfenghui-jf-score-service/src/main/java/com/yunfenghui/jf/score/util/ScoreMessageCode.java
@@ -0,0 +1,8 @@
+package com.yunfenghui.jf.score.util;
+
+public interface ScoreMessageCode {
+	/**
+	 * 商户库存积分余额不足
+	 */
+	String PARTNER_STOCK_SCORE_NOT_ENOUGH = "jf.score.service.partner.stock.score.not.enough";
+}
