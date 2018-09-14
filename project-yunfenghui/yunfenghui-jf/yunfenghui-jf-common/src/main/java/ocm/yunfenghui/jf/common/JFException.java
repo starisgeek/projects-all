a58d0b55c2bed2afa2c8c64199b28dcc1e2ab9a6
cloud_wanghao <wanghao_oo@126.com>
2018-09-14 10:17:42 +0800

    JFException

---
+++ project-yunfenghui/yunfenghui-jf/yunfenghui-jf-common/src/main/java/ocm/yunfenghui/jf/common/JFException.java
@@ -0,0 +1,36 @@
+package ocm.yunfenghui.jf.common;
+
+public class JFException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	private final String errorCode;
+
+	private final Object[] args;
+
+	public JFException(String errorCode) {
+		this(errorCode, null);
+	}
+
+	public JFException(String errorCode, String message) {
+		this(errorCode, message, null);
+	}
+
+	public JFException(String errorCode, String message, Throwable cause) {
+		this(errorCode, message, cause, null);
+	}
+
+	public JFException(String errorCode, String message, Throwable cause, Object[] args) {
+		super(message, cause);
+		this.errorCode = errorCode;
+		this.args = args;
+	}
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	public Object[] getArgs() {
+		return args;
+	}
+
+}
