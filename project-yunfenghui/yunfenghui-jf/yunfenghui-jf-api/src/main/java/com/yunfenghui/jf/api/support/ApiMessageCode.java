ab2087d78b0b59fe708d4eedb52d6bc98c426ae2
cloud_wanghao <wanghao_oo@126.com>
2018-09-14 15:20:00 +0800

    MessageCode

---
+++ project-yunfenghui/yunfenghui-jf/yunfenghui-jf-api/src/main/java/com/yunfenghui/jf/api/support/ApiMessageCode.java
@@ -0,0 +1,19 @@
+package com.yunfenghui.jf.api.support;
+
+public interface ApiMessageCode {
+
+	/**
+	 * 缺少参数
+	 */
+	String PARAM_MISSING = "jf.api.param.missing";
+
+	/**
+	 * 无效的appId
+	 */
+	String APP_ID_INVALID = "jf.api.appid.invalid";
+
+	/**
+	 * 签名错误
+	 */
+	String SIGN_ERROR = "jf.api.sign.error";
+}
