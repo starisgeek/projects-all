2dbc9f56b45ecde7501359b125ec42d655c0e51d
cloud_wanghao <wanghao_oo@126.com>
2018-09-17 19:41:03 +0800

    增加CHARSET_UTF8常量

--- project-yunfenghui/yunfenghui-common/src/main/java/com/yunfenghui/common/Commons.java
+++ project-yunfenghui/yunfenghui-common/src/main/java/com/yunfenghui/common/Commons.java
@@ -1,9 +1,11 @@
 package com.yunfenghui.common;
 
+import java.nio.charset.Charset;
 import java.util.Random;
 
 public abstract class Commons {
 	public static final String UTF8 = "UTF-8";
+	public static final Charset CHARSET_UTF8 = Charset.forName(UTF8);
 
 	/**
 	 * 生成指定长度的随机数字符串
