bd1ffc39452504d602a04b5c6efb1b8c8ae60b47
cloud_wanghao <wanghao_oo@126.com>
2018-09-27 14:53:03 +0800

    修改goods为goodsId

--- project-yunfenghui/yunfenghui-erp/yunfenghui-erp-stock-service/src/main/java/com/yunfenghui/erp/stock/model/StockRecordItem.java
+++ project-yunfenghui/yunfenghui-erp/yunfenghui-erp-stock-service/src/main/java/com/yunfenghui/erp/stock/model/StockRecordItem.java
@@ -1,11 +1,87 @@
 package com.yunfenghui.erp.stock.model;
 
+import java.io.Serializable;
+
 /**
  * 进货记录明细
  * 
  * @author Administrator
  *
  */
-public class StockRecordItem {
+public class StockRecordItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 入库单号
+	 */
+	private String recordNo;
+	/**
+	 * 商品
+	 */
+	private int goodsId;
 
+	/**
+	 * 进价
+	 */
+	private int buyPrice;
+	/**
+	 * 进货量
+	 */
+	private int buyQuantity;
+	/**
+	 * 赠送量
+	 */
+	private int presentQuantity;
+
+	/**
+	 * 总金额
+	 */
+	private int totalAmount;
+
+	public String getRecordNo() {
+		return recordNo;
+	}
+
+	public void setRecordNo(String recordNo) {
+		this.recordNo = recordNo;
+	}
+
+	public int getGoodsId() {
+		return goodsId;
+	}
+
+	public void setGoodsId(int goodsId) {
+		this.goodsId = goodsId;
+	}
+
+	public int getBuyPrice() {
+		return buyPrice;
+	}
+
+	public void setBuyPrice(int buyPrice) {
+		this.buyPrice = buyPrice;
+	}
+
+	public int getBuyQuantity() {
+		return buyQuantity;
+	}
+
+	public void setBuyQuantity(int buyQuantity) {
+		this.buyQuantity = buyQuantity;
+	}
+
+	public int getPresentQuantity() {
+		return presentQuantity;
+	}
+
+	public void setPresentQuantity(int presentQuantity) {
+		this.presentQuantity = presentQuantity;
+	}
+
+	public int getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(int totalAmount) {
+		this.totalAmount = totalAmount;
+	}
 }
