218d62f318f47262a91a39d1f267447c8f077f74
cloud_wanghao <wanghao_oo@126.com>
2018-09-27 17:19:20 +0800

    新增erp.goods.not.exist.or.status.error

--- project-yunfenghui/yunfenghui-erp/yunfenghui-erp-goods-service/src/main/java/com/yunfenghui/erp/goods/util/GoodsMessageCode.java
+++ project-yunfenghui/yunfenghui-erp/yunfenghui-erp-goods-service/src/main/java/com/yunfenghui/erp/goods/util/GoodsMessageCode.java
@@ -4,10 +4,15 @@ public interface GoodsMessageCode {
 	/**
 	 * 商品名称已存在
 	 */
-	String GOODS_NAME_EXISTS = "erp.goods.service.name.exists";
+	String GOODS_NAME_EXISTS = "erp.goods.name.exists";
 
 	/**
 	 * 商品条码已存在
 	 */
-	String GOODS_BARCODE_EXISTS = "erp.goods.service.barcode.exists";
+	String GOODS_BARCODE_EXISTS = "erp.goods.barcode.exists";
+
+	/**
+	 * 商品不存在或状态错误
+	 */
+	String GOODS_NOT_EXISTS_OR_STATUS_ERROR = "erp.goods.not.exist.or.status.error";
 }
