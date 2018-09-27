76bfd1c0fdac2778fa01e545b12bb9a3b0ac2346
cloud_wanghao <wanghao_oo@126.com>
2018-09-27 20:13:10 +0800

    新增erp.goods.not.exists

--- project-yunfenghui/yunfenghui-erp/yunfenghui-erp-goods-service/src/main/java/com/yunfenghui/erp/goods/util/GoodsMessageCode.java
+++ project-yunfenghui/yunfenghui-erp/yunfenghui-erp-goods-service/src/main/java/com/yunfenghui/erp/goods/util/GoodsMessageCode.java
@@ -12,6 +12,11 @@ public interface GoodsMessageCode {
 	String GOODS_BARCODE_EXISTS = "erp.goods.barcode.exists";
 
 	/**
+	 * 商品不存在
+	 */
+	String GOODS_NOT_EXISTS = "erp.goods.not.exists";
+
+	/**
 	 * 商品不存在或状态错误
 	 */
 	String GOODS_NOT_EXISTS_OR_STATUS_ERROR = "erp.goods.not.exist.or.status.error";
