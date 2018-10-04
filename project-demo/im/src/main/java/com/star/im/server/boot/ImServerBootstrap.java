93e37f1660fb671dbf0d68a7a5ca981d495c92f3
cloud_wanghao <wanghao_oo@126.com>
2018-10-04 11:27:57 +0800

    ImServer启动类

---
+++ project-demo/im/src/main/java/com/star/im/server/boot/ImServerBootstrap.java
@@ -0,0 +1,45 @@
+package com.star.im.server.boot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.star.im.util.ApplicationConfigurer;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+public class ImServerBootstrap {
+	private static final Logger logger = LoggerFactory.getLogger(ImServerBootstrap.class);
+	private static final String KEY_IM_SERVER_PORT = "im.server.port";
+
+	public static void main(String[] args) throws Exception {
+		NioEventLoopGroup boss = new NioEventLoopGroup(1);
+		NioEventLoopGroup worker = new NioEventLoopGroup();
+
+		ServerBootstrap boot = new ServerBootstrap();
+		boot.group(boss, worker).channel(NioServerSocketChannel.class)
+				.childHandler(new ChannelInitializer<Channel>() {
+					@Override
+					protected void initChannel(Channel ch) throws Exception {
+
+					}
+				});
+		ChannelFuture f = boot
+				.bind(Integer.valueOf(ApplicationConfigurer.valueOf(KEY_IM_SERVER_PORT)))
+				.addListener(new GenericFutureListener<Future<? super Void>>() {
+					@Override
+					public void operationComplete(Future<? super Void> future) throws Exception {
+						if (future.isSuccess()) {
+							logger.info("ImServer bootstrap success");
+						}
+					}
+				});
+		f.sync();
+	}
+}
