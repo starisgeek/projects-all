92aa4440c6cb2073c4e41300eb164902e4f3c820
cloud_wanghao <wanghao_oo@126.com>
2018-10-05 10:14:52 +0800

    MessageCode常量接口

---
+++ project-yunfenghui/yunfenghui-erp/yunfenghui-erp-order-service/src/main/java/com/yunfenghui/erp/order/util/OrderMessageCode.java
@@ -0,0 +1,8 @@
+package com.yunfenghui.erp.order.util;
+
+public interface OrderMessageCode {
+	/**
+	 * 订单不存在或者状态错误
+	 */
+	String ORDER_NOT_EXISTS_OR_STATUS_ERROR = "erp.order.not.exists.or.status.error";
+}
