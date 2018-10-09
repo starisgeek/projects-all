8ff56f821eaa62734a1122fc0773f7e5ec766606
cloud_wanghao <wanghao_oo@126.com>
2018-10-09 14:29:18 +0800

    增加increaseRefundQuantityAndRefundAmount,decreaseRefundQuantityAndRefundAmount

--- project-yunfenghui/yunfenghui-erp/yunfenghui-erp-order-service-impl/src/main/java/com/yunfenghui/erp/order/dao/OrderItemDao.java
+++ project-yunfenghui/yunfenghui-erp/yunfenghui-erp-order-service-impl/src/main/java/com/yunfenghui/erp/order/dao/OrderItemDao.java
@@ -15,4 +15,32 @@ public interface OrderItemDao {
 	 * @param items
 	 */
 	void batchInsertOrderItems(@Param("items") List<OrderItem> items);
+
+	/**
+	 * 增加退款金额和数量
+	 * 
+	 * @param orderNo
+	 * @param goodsId
+	 * @param refundQuantityIncrement
+	 * @param refundAmountIncrement
+	 * @return
+	 */
+	int increaseRefundQuantityAndRefundAmount(@Param("orderNo") String orderNo,
+			@Param("goodsId") int goodsId,
+			@Param("refundQuantityIncrement") int refundQuantityIncrement,
+			@Param("refundAmountIncrement") int refundAmountIncrement);
+
+	/**
+	 * 减少退款金额和数量
+	 * 
+	 * @param orderNo
+	 * @param goodsId
+	 * @param refundQuantityDecrement
+	 * @param refundAmountDecrement
+	 * @return
+	 */
+	int decreaseRefundQuantityAndRefundAmount(@Param("orderNo") String orderNo,
+			@Param("goodsId") int goodsId,
+			@Param("refundQuantityDecrement") int refundQuantityDecrement,
+			@Param("refundAmountDecrement") int refundAmountDecrement);
 }
