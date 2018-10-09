d32ce83939d58b7f2c31e2a48c50d17ff545cc42
cloud_wanghao <wanghao_oo@126.com>
2018-10-09 22:55:05 +0800

    增加MESSAGE_REQUEST,MESSAGE_RESPONSE映射

--- project-demo/im/src/main/java/com/star/im/util/CommandMapping.java
+++ project-demo/im/src/main/java/com/star/im/util/CommandMapping.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 import com.star.im.entity.LoginRequest;
 import com.star.im.entity.LoginResponse;
+import com.star.im.entity.MessageRequest;
+import com.star.im.entity.MessageResponse;
 import com.star.im.entity.Packet;
 
 public class CommandMapping {
@@ -15,6 +17,8 @@ public class CommandMapping {
 		Map<Byte, Class<? extends Packet>> map = new HashMap<>();
 		map.put(Commands.LOGIN_REQUEST, LoginRequest.class);
 		map.put(Commands.LOGIN_RESPONSE, LoginResponse.class);
+		map.put(Commands.MESSAGE_REQUEST, MessageRequest.class);
+		map.put(Commands.MESSAGE_RESPONSE, MessageResponse.class);
 
 		mapping = Collections.unmodifiableMap(map);
 	}
