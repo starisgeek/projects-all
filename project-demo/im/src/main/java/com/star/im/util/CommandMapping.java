694969f38b0ce740a46f4b63326564fd6bbb9445
cloud_wanghao <wanghao_oo@126.com>
2018-10-09 22:28:18 +0800

    Command和Packet class映射

---
+++ project-demo/im/src/main/java/com/star/im/util/CommandMapping.java
@@ -0,0 +1,25 @@
+package com.star.im.util;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.star.im.entity.LoginRequest;
+import com.star.im.entity.LoginResponse;
+import com.star.im.entity.Packet;
+
+public class CommandMapping {
+	private static final Map<Byte, Class<? extends Packet>> mapping;
+
+	static {
+		Map<Byte, Class<? extends Packet>> map = new HashMap<>();
+		map.put(Commands.LOGIN_REQUEST, LoginRequest.class);
+		map.put(Commands.LOGIN_RESPONSE, LoginResponse.class);
+
+		mapping = Collections.unmodifiableMap(map);
+	}
+
+	public static Class<? extends Packet> packetTypeOf(byte command) {
+		return mapping.get(command);
+	}
+}
