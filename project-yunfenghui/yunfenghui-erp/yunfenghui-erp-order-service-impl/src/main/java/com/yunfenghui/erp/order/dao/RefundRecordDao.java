4004459b9638fb4d0ff2223012abe2b533b4b575
cloud_wanghao <wanghao_oo@126.com>
2018-10-09 16:11:03 +0800

    增加updateRefundRecordStatusAndOutRefundTradeNo方法

--- project-yunfenghui/yunfenghui-erp/yunfenghui-erp-order-service-impl/src/main/java/com/yunfenghui/erp/order/dao/RefundRecordDao.java
+++ project-yunfenghui/yunfenghui-erp/yunfenghui-erp-order-service-impl/src/main/java/com/yunfenghui/erp/order/dao/RefundRecordDao.java
@@ -1,5 +1,8 @@
 package com.yunfenghui.erp.order.dao;
 
+import java.util.Date;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.yunfenghui.erp.order.model.RefundRecord;
@@ -12,4 +15,19 @@ public interface RefundRecordDao {
 	 * @param record
 	 */
 	void insertRefundRecord(RefundRecord record);
+
+	/**
+	 * 修改RefundRecord状态和outRefundTradeNo
+	 * 
+	 * @param refundRecordNo
+	 * @param expectedStatus
+	 * @param updatedStatus
+	 * @param outRefundTradeNo
+	 * @param modifyTime
+	 * @return
+	 */
+	int updateRefundRecordStatusAndOutRefundTradeNo(@Param("refundRecordNo") String refundRecordNo,
+			@Param("expectedStatus") int expectedStatus, @Param("updatedStatus") int updatedStatus,
+			@Param("outRefundTradeNo") String outRefundTradeNo,
+			@Param("modifyTime") Date modifyTime);
 }
