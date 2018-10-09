e110f68ff42fce3138adce60a4ea22a994fd1c96
cloud_wanghao <wanghao_oo@126.com>
2018-10-09 10:35:40 +0800

    增加ORDER_NOT_EXISTS常量

--- project-yunfenghui/yunfenghui-erp/yunfenghui-erp-order-service/src/main/java/com/yunfenghui/erp/order/util/OrderMessageCode.java
+++ project-yunfenghui/yunfenghui-erp/yunfenghui-erp-order-service/src/main/java/com/yunfenghui/erp/order/util/OrderMessageCode.java
@@ -2,6 +2,10 @@ package com.yunfenghui.erp.order.util;
 
 public interface OrderMessageCode {
 	/**
+	 * 订单不存在
+	 */
+	String ORDER_NOT_EXISTS = "erp.order.not.exists";
+	/**
 	 * 订单不存在或者状态错误
 	 */
 	String ORDER_NOT_EXISTS_OR_STATUS_ERROR = "erp.order.not.exists.or.status.error";
