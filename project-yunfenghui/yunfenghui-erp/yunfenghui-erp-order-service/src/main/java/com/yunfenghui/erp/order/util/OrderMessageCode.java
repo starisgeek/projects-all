4c97d38773a2c0afe505c4755040c1912de35cc2
cloud_wanghao <wanghao_oo@126.com>
2018-10-09 11:46:22 +0800

    增加常量REFUND_ORDER_ITEM_QUANTITY_OVERFLOW,REFUND_ORDER_ITEM_NOT_FOUND_IN_ORDER

--- project-yunfenghui/yunfenghui-erp/yunfenghui-erp-order-service/src/main/java/com/yunfenghui/erp/order/util/OrderMessageCode.java
+++ project-yunfenghui/yunfenghui-erp/yunfenghui-erp-order-service/src/main/java/com/yunfenghui/erp/order/util/OrderMessageCode.java
@@ -9,4 +9,13 @@ public interface OrderMessageCode {
 	 * 订单不存在或者状态错误
 	 */
 	String ORDER_NOT_EXISTS_OR_STATUS_ERROR = "erp.order.not.exists.or.status.error";
+	/**
+	 * 退款订单项数量超出购买数量
+	 */
+	String REFUND_ORDER_ITEM_QUANTITY_OVERFLOW = "erp.refund.order.item.quantity.overflow";
+
+	/**
+	 * 退款订单项在原订单中不存在
+	 */
+	String REFUND_ORDER_ITEM_NOT_FOUND_IN_ORDER = "erp.refund.order.item.not.found.in.order";
 }
