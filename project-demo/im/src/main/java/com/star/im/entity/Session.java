b9ff85bd6c6c7689a73eca94f635e7116585588f
cloud_wanghao <wanghao_oo@126.com>
2018-10-11 22:20:48 +0800

    增加ATTRIBUTE_KEY

--- project-demo/im/src/main/java/com/star/im/entity/Session.java
+++ project-demo/im/src/main/java/com/star/im/entity/Session.java
@@ -1,8 +1,12 @@
 package com.star.im.entity;
 
+import io.netty.util.AttributeKey;
+
 public class Session {
 	private final String userId;
 	private final String username;
+
+	public static final AttributeKey<Session> ATTRIBUTE_KEY = AttributeKey.newInstance("session");
 
 	public Session(String userId, String username) {
 		this.userId = userId;
