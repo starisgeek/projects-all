5cbb99b162e6a62acb9aefff3dbd60a7bb6ff427
cloud_wanghao <wanghao_oo@126.com>
2018-10-14 15:48:17 +0800

    退出群请求Handler

---
+++ project-demo/im/src/main/java/com/star/im/handler/QuitGroupRequestHandler.java
@@ -0,0 +1,34 @@
+package com.star.im.handler;
+
+import com.star.im.entity.QuitGroupRequest;
+import com.star.im.entity.QuitGroupResponse;
+import com.star.im.util.SessionManager;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+
+public class QuitGroupRequestHandler extends SimpleChannelInboundHandler<QuitGroupRequest> {
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, QuitGroupRequest msg) throws Exception {
+		String groupId = msg.getGroupId();
+		ChannelGroup channelGroup = SessionManager.getChannelGroup(groupId);
+		QuitGroupResponse response = new QuitGroupResponse();
+		if (channelGroup != null) {
+			response.setGroupId(groupId);
+			boolean quit = channelGroup.remove(ctx.channel());
+			if (quit) {
+				response.setSuccess(true);
+			} else {
+				response.setSuccess(false);
+				response.setReason("该用户还未加入群:" + groupId);
+			}
+		} else {
+			response.setSuccess(false);
+			response.setReason("群id:" + groupId + "不存在");
+		}
+		ctx.channel().writeAndFlush(response);
+	}
+
+}
