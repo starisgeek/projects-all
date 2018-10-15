a3ae5877ebcdbcc9e60e9951c32b4fa2e97739ed
cloud_wanghao <wanghao_oo@126.com>
2018-10-15 22:01:58 +0800

    magic number验证的LengthFieldBasedFrameDecoder

---
+++ project-demo/im/src/main/java/com/star/im/handler/ImLengthFieldDecoder.java
@@ -0,0 +1,30 @@
+package com.star.im.handler;
+
+import com.star.im.entity.Packet;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+public class ImLengthFieldDecoder extends LengthFieldBasedFrameDecoder {
+	private static final int LENGTH_FIELD_OFFSET = 7;
+	private static final int LENGTH_FIELD_LENGTH = 4;
+
+	public ImLengthFieldDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength) {
+		super(Integer.MAX_VALUE, LENGTH_FIELD_OFFSET, LENGTH_FIELD_LENGTH);
+	}
+
+	@Override
+	protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+		if (validateMagicNumber(in)) {
+			return super.decode(ctx, in);
+		} else {
+			ctx.channel().close();
+			return null;
+		}
+	}
+
+	private boolean validateMagicNumber(ByteBuf in) {
+		return in.getInt(in.readerIndex()) == Packet.MAGIC_NUMBER;
+	}
+}
