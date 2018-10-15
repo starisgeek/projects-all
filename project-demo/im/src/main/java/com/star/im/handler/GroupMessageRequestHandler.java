2a8e9741f9dcd094fcbff3322a42d656fbb0f12a
cloud_wanghao <wanghao_oo@126.com>
2018-10-15 22:52:03 +0800

    群消息请求Handler

---
+++ project-demo/im/src/main/java/com/star/im/handler/GroupMessageRequestHandler.java
@@ -0,0 +1,30 @@
+package com.star.im.handler;
+
+import com.star.im.entity.GroupMessageRequest;
+import com.star.im.entity.GroupMessageResponse;
+import com.star.im.util.SessionManager;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+
+public class GroupMessageRequestHandler extends SimpleChannelInboundHandler<GroupMessageRequest> {
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, GroupMessageRequest msg)
+			throws Exception {
+		String groupId = msg.getGroupId();
+		String message = msg.getMessage();
+
+		ChannelGroup channelGroup = SessionManager.getChannelGroup(groupId);
+		if (channelGroup != null) {
+			String fromUsername = SessionManager.getSession(ctx.channel()).getUsername();
+			GroupMessageResponse response = new GroupMessageResponse();
+			response.setFromUsername(fromUsername);
+			response.setGroupId(groupId);
+			response.setMessage(message);
+			channelGroup.writeAndFlush(response);
+		}
+	}
+
+}
